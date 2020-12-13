b87722b56693468a7ad033fd9f937bf2b3052452
André Filipe Valente Vital <andre.vital@zatio.com>
2020-12-12 21:40:40 -0300

    added more external api keys

--- back-end/StockWatcherApi/utils/getStockInfo.py
+++ back-end/StockWatcherApi/utils/getStockInfo.py
@@ -2,7 +2,11 @@ import json
 import requests
 
 def getStockInfo(ticker):
-    apiKey = '8e4e0e07'
-    url = f'https://api.hgbrasil.com/finance/stock_price?key={apiKey}&symbol={ticker}'
-    response = requests.get(url)
-    return json.loads(response.text)
+    apiKeys = ['a6f8438a', '3a7911d9', '376a08aa', '8e4e0e07']
+    for apiKey in apiKeys:
+        url = f'https://api.hgbrasil.com/finance/stock_price?key={apiKey}&symbol={ticker}'
+        responseJson = requests.get(url)
+        response = json.loads(responseJson.text)
+        if response['results']:
+            return response
+        
